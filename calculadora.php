1a84eda1293095e8e78f008cf5f44ea2f76db734
laercioss <71737006+laercioss@users.noreply.github.com>
2024-10-05 14:44:58 +0000

    adicionar calculadora PHP

---
+++ calculadora.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+</head>
+<body>
+    <h1>Calculadora Fullstack</h1>
+
+    <form action="calculadora.php" method="post">
+        <label for="">Número 1</label>
+        <input type="text" name="num1" id="num1">
+
+        <label for="">Número 2</label>
+        <input type="text" name="num2" id="num1">
+
+        <input type="submit" value="Calcular">
+    </form>
+
+    <hr/>
+    <p>Resultado da operação</p>
+    <?php
+        if(empty)
+    ?>
+</body>
+</html>
